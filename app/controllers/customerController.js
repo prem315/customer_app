b55375364d9f87924663b4acce5e934e3dfaa053
Prem Patel <prem.patel3105@gmail.com>
2016-02-28 14:36:39 +0530

    complete orderBy on customer table

--- app/controllers/customerController.js
+++ app/controllers/customerController.js
@@ -1,9 +1,17 @@
 myApp.controller('customerController', function($scope){
 
+	$scope.sortBy = 'name';
+	$scope.reverse = false;
+
 	$scope.customers = [
 		{joined: '2012-12-4', name: 'Prem', city: 'Ahmedabad', orderTotal: '3'},
 		{joined: '2011-12-4', name: 'Gurdeep', city: 'Jammu', orderTotal: '2'},
 		{joined: '2013-12-4', name: 'Paras', city: 'Delhi', orderTotal: '5'},
 		{joined: '2015-12-4', name: 'Guruji', city: 'Mumbai', orderTotal: '7'}
 	];
+
+	$scope.doSort = function(propName){
+		$scope.sortBy = propName;
+		$scope.reverse = !$scope.reverse;
+	};
 })
